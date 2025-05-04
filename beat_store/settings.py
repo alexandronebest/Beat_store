ced8491e70e5f78156391776ef3dbe34c97ba3de
alexandronebest <alex-domrachev@mail.ru>
2025-05-04 09:19:02 +0700

    поменял в сетингс путь к ксс

--- beat_store/settings.py
+++ beat_store/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files (uploaded by users)
